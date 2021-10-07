af10c8255bb6844619abaf91ebbca8d811f83595
Bilal Sarimeseli <45982392+bilalsarimeseli@users.noreply.github.com>
2021-10-06 20:33:04 -0700

    form.js updated
    DatePicker Component added

--- src/InputForm/form.js
+++ src/InputForm/form.js
@@ -1,45 +1,53 @@
 import React from "react";
+import "react-datepicker/dist/react-datepicker.css";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
+import { DatePicker } from '@material-ui/pickers'
+
 
 export const Form = (props) => {
   return (
-   <form onSubmit={() => {}}>
-     <TextField
-       id="name"
-       name="name"
-       label="Name"
-       fullWidth
+    <form onSubmit={() => { }}>
+      <TextField
+        id="name"
+        name="name"
+        label="Name"
+        margin="normal"
+        fullWidth
 
-     />
-     <TextField
-       id="email"
-       name="email"
-       label="Email"
-       fullWidth
-     />
-     <TextField
-       id="password"
-       name="password"
-       label="Password"
-       fullWidth
-       type="password"
-     />
-     <TextField
-       id="confirmPassword"
-       name="confirmPassword"
-       label="Confirm Password"
-       fullWidth
-       type="password"
-     />
-     <Button
-       type="submit"
-       fullWidth
-       variant="raised"
-       color="primary"
-     >
-       Submit
-     </Button>
-   </form>
- );
+      />
+      <DatePicker
+        id="date"
+        name="date"
+        label="Date"
+        margin="normal"
+        fullWidth
+      />
+
+      <TextField
+        id="active"
+        name="active"
+        label="Active"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        id="age"
+        name="age"
+        label="Age"
+        margin="normal"
+        fullWidth
+      />
+
+      <Button
+        type="submit"
+        fullWidth
+        variant="raised"
+        color="primary"
+        margin="normal"
+      >
+        Submit
+      </Button>
+    </form>
+  );
 };
