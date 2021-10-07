82ee16d7ea2e1cb562f7c1712f71994a9cdcc889
Bilal Sarimeseli <45982392+bilalsarimeseli@users.noreply.github.com>
2021-10-06 20:32:08 -0700

    Updated index.js

--- src/InputForm/index.js
+++ src/InputForm/index.js
@@ -5,40 +5,40 @@ import { Form } from "./form";
 import Paper from "@material-ui/core/Paper";
 
 const styles = theme => ({
- paper: {
-   marginTop: theme.spacing.unit * 8,
-   display: "flex",
-   flexDirection: "column",
-   alignItems: "center",
-   padding: `${theme.spacing.unit * 5}px ${theme.spacing.unit * 5}px ${theme
-     .spacing.unit * 5}px`
- },
- container: {
-   maxWidth: "200px"
- }
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: `${theme.spacing.unit * 5}px ${theme.spacing.unit * 5}px ${theme
+      .spacing.unit * 5}px`
+  },
+  container: {
+    maxWidth: "200px"
+  }
 });
 
 class InputForm extends Component {
- constructor(props) {
-   super(props);
-   this.state = {};
- }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
- render() {
-   const classes = this.props;
-   return (
-     <React.Fragment>
-          <div className={classes.container}>
-         <Paper elevation={1} className={classes.paper}>
-           <h1>Form</h1>
-           <Formik
-             render={props => <Form {...props} />}
-           />
-         </Paper>
-       </div>
-     </React.Fragment>
-   );
- }
+  render() {
+    const classes = this.props;
+    return (
+      <React.Fragment>
+        <div className={classes.container}>
+          <Paper elevation={1} className={classes.paper}>
+            <h1>Form</h1>
+            <Formik
+              render={props => <Form {...props} />}
+            />
+          </Paper>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default withStyles(styles)(InputForm);
